9af30139e69ba49186ec9527e29b2c04fc6e5de9
Tiago <tnthiagolima88@gmail.com>
2021-08-28 14:57:44 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com um Número Inteiro");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
